1c0cfdf25d7c0f33e33c9753803ab9c629d6e202
ekirshey <ekirshey@gmail.com>
2016-08-10 22:29:17 -0400

    Added login page template

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/login');
 });
 
 app.get('/league', function(request, response) {
